fb14fcb2f5c2be46f5e68d0ed04ea10ea4a90012
baguinaga <42448094+Baguinaga@users.noreply.github.com>
2018-10-28 23:48:07 -0400

    working routes/orm

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (name) 
+VALUES 
+("Bob's Burger"),
+("Cheeseburger"),
+("Tofu Burger (yuck)"),
+("Krabby Patty"),
+("Last Night's Leftover Burger"),
+("Big Mac");
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controllers");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(routes);
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});

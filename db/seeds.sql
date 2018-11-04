bf38507c7369a197e8e2ef42316ded9e94ce2c85
baguinaga <42448094+Baguinaga@users.noreply.github.com>
2018-11-04 15:30:17 -0500

    deploying to heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO burgers (name) 
+INSERT INTO burgers (burger_name, devoured) 
 VALUES 
-("Bob's Burger"),
-("Cheeseburger"),
-("Tofu Burger (yuck)"),
-("Krabby Patty"),
-("Last Night's Leftover Burger"),
-("Big Mac");
+("Bob's Burger", true),
+("Cheeseburger", true),
+("Tofu Burger (yuck)", false),
+("Krabby Patty", true),
+("Last Night's Leftover Burger", true),
+("Big Mac", true);
